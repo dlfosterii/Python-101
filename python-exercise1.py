f5a4acace5e64bb5bafa0bd196a33e1434861570
David Foster <dlfosterii@yahoo.com>
2020-04-01 11:26:00 -0400

    Creating initial files

---
+++ python-exercise1.py
@@ -0,0 +1,11 @@
+# Exercise 1 creating a "Hello you!" print out
+
+#gathering information
+name = input('Tell me your name: ')
+print(f'Hello, {name}')
+
+
+
+
+#printing output
+print(f'Hello, {name}')
